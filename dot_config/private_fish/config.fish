4450eb8fc6336977e13902ed7d9dca63f59b3a85
Andrew Lee <candrewlee14@gmail.com>
2024-01-17 12:12:28 -0700

    add atuin to fish config and fix is-interactive content

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,13 +1,14 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
+    zoxide init fish | source
+    atuin init fish | source
+
+    if test -f ~/.config/fish/config.fish.local
+        source ~/.config/fish/config.fish.local
+    end
 end
 
 fish_add_path $HOME/.webman/bin
 
 set -gx EDITOR nvim
 
-zoxide init fish | source
-
-if test -f ~/.config/fish/config.fish.local
-    source ~/.config/fish/config.fish.local
-end
