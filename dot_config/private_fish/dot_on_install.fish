edcd1506621a86e25286d9711541d34602080be1
Andrew Lee <candrewlee14@gmail.com>
2024-03-27 19:22:21 -0600

    improve fisher & atuin installation text

--- dot_config/private_fish/dot_on_install.fish
+++ dot_config/private_fish/dot_on_install.fish
@@ -1,10 +1,12 @@
 # if fisher is not installed, install it
 if not functions -q fisher
     curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
-end
-if functions -q fisher
-    fisher install IlanCosman/tide@v6
-    fisher install PatrickF1/fzf.fish
+    if not functions -q fisher
+        echo "fisher failed to install"
+    else
+        fisher install IlanCosman/tide@v6
+        fisher install PatrickF1/fzf.fish
+    end
 end
 
 fish_add_path ~/.webman/bin/
@@ -12,7 +14,9 @@ fish_add_path ~/.webman/bin/
 # if atuin is not installed, install it
 if not type -q atuin
     bash (curl https://raw.githubusercontent.com/atuinsh/atuin/main/install.sh | psub)
-end
-if type -q atuin
-    atuin import auto
+    if not type -q atuin
+        echo "atuin failed to install"
+    else
+        atuin import auto
+    end
 end
