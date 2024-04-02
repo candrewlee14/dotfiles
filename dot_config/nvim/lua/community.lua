717e88ea06203a0aebe34825c54366494b195a0b
Andrew Lee <candrewlee14@gmail.com>
2024-04-02 00:03:38 -0600

    update to AstroNvim v4

---
+++ dot_config/nvim/lua/community.lua
@@ -0,0 +1,43 @@
+if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+
+-- AstroCommunity: import any community modules here
+-- We import this file in `lazy_setup.lua` before the `plugins/` folder.
+-- This guarantees that the specs are processed before any user plugins.
+
+---@type LazySpec
+return {
+  "AstroNvim/astrocommunity",
+  { import = "astrocommunity.pack.lua" },
+  -- import/override with your plugins folder
+
+  { import = "astrocommunity.colorscheme.catppuccin" },
+  -- { import = "astrocommunity.editing-support.yanky-nvim" },
+  { import = "astrocommunity.diagnostics.trouble-nvim"},
+  { import = "astrocommunity.editing-support.todo-comments-nvim" },
+  { import = "astrocommunity.editing-support.multicursors-nvim" },
+  { import = "astrocommunity.lsp.lsp-signature-nvim" },
+  { import = "astrocommunity.lsp.inc-rename-nvim" },
+  { import = "astrocommunity.markdown-and-latex.glow-nvim" },
+  -- { import = "astrocommunity.motion.flash-nvim" },
+  { import = "astrocommunity.scrolling.mini-animate" },
+  { import = "astrocommunity.syntax.vim-sandwich" },
+  { import = "astrocommunity.test.neotest" },
+  -- { import = "astrocommunity.workflow.hardtime-nvim" },
+
+  { import = "astrocommunity.completion.copilot-lua" },
+  { -- further customize the options set by the community
+    "zbirenbaum/copilot.lua",
+    opts = {
+      suggestion = {
+        keymap = {
+          accept = "<C-l>",
+          accept_word = false,
+          accept_line = false,
+          next = "<C-.>",
+          prev = "<C-,>",
+          dismiss = "<C/>",
+        },
+      },
+    },
+  },
+}
