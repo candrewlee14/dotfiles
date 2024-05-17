e5c3667a18c8e9d7e95db8ff764be4ea37bff1fc
Andrew Lee <candrewlee14@gmail.com>
2024-05-17 08:14:12 -0600

    suppress write messages for noice.nvim

--- dot_config/nvim/lua/community.lua
+++ dot_config/nvim/lua/community.lua
@@ -58,4 +58,24 @@ return {
       },
     },
   },
+  {
+   "folke/noice.nvim",
+    opts = {
+      routes = {
+        {
+          filter = {
+            event = 'msg_show',
+            any = {
+              { find = '%d+L, %d+B' },
+              { find = '; after #%d+' },
+              { find = '; before #%d+' },
+              { find = '%d fewer lines' },
+              { find = '%d more lines' },
+            },
+          },
+          opts = { skip = true },
+        }
+      }
+    }
+  }
 }
