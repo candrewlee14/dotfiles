0e51328e1a6412883403853fe9fc06e359c24c02
Andrew Lee <candrewlee14@gmail.com>
2026-09-06 17:30:54 -0600

    feat(nvim): switch to LazyVim native copilot extra with <C-l> accept

--- dot_config/nvim/lua/plugins/copilot.lua
+++ dot_config/nvim/lua/plugins/copilot.lua
@@ -1,31 +1,25 @@
 return {
   {
     "zbirenbaum/copilot.lua",
-    cmd = "Copilot",
-    event = "InsertEnter",
+    opts = function(_, opts)
+      opts.suggestion = opts.suggestion or {}
+      opts.suggestion.keymap = opts.suggestion.keymap or {}
+      -- Support <C-l> in addition to LazyVim's native <Tab> accept
+      opts.suggestion.keymap.accept = "<C-l>"
+    end,
+  },
+  {
+    "saghen/blink.cmp",
+    optional = true,
     opts = {
-      suggestion = {
-        enabled = true,
-        auto_trigger = true,
-        keymap = {
-          accept = "<C-l>",
-          accept_word = false,
-          accept_line = false,
-          next = "<M-]>",
-          prev = "<M-[>",
-          dismiss = "<C-/>",
+      keymap = {
+        ["<C-l>"] = {
+          LazyVim.cmp.map({ "snippet_forward", "ai_nes", "ai_accept" }),
+          "select_and_accept",
+          "fallback",
         },
       },
-      panel = { enabled = false },
     },
-    config = function(_, opts)
-      require("copilot").setup(opts)
-      vim.keymap.set("i", "<C-]>", function()
-        if require("copilot.suggestion").is_visible() then
-          require("copilot.suggestion").next()
-        end
-      end, { desc = "[copilot] next suggestion" })
-    end,
   },
 }
 
